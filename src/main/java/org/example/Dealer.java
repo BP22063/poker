8a6a09425f6193f2f5838acb6491b1a6ebcb6023
bp22029 <bp22029@shibaura-it.ac.jp>
2024-12-24 14:38:59 +0900

    Add Player class Dealerクラスを編集

---
+++ src/main/java/org/example/Dealer.java
@@ -0,0 +1,116 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Dealer {
+
+    private ArrayList<Player> players;
+    private int fieldbetChip;
+    private Deck deck;
+    private ArrayList<Player> rankList;
+    private ArrayList<String> disableHands;
+    private ArrayList<Integer> usedSkills;
+
+    //private Skill_handSwap  skill_handSwap ;
+
+    //private Skill_disableHand skill_disableHand;
+
+
+    //private Skill_exchangingHandsAgain skill_exchangingHandsAgain;
+
+    public Dealer() {
+        players = new ArrayList<>();
+        deck = new Deck();
+        deck.shuffle();
+    }
+
+    public void decideOrder() {
+        players.add(players.remove(0));
+    }
+
+    public void provideSkill(int Skill) {
+
+    }
+
+    public void collectInitialChip() {
+
+    }
+
+    public void provideCard() {
+
+    }
+
+    public void changeCard(int playerIndex, int cardIndex) {
+        if (playerIndex >= 0 && playerIndex < players.size()) {
+            Player player = players.get(playerIndex);
+            Card oldCard = player.hand.get(cardIndex);
+            Card newCard = deck.draw();
+
+            player.exchangeCard_player(cardIndex, newCard);
+            deck.discard(oldCard);  // 捨て札に追加
+        }
+    }
+
+    /*
+    public void provideChip(Player players) {
+
+    }
+
+    */
+
+    private void adaptSkill() {
+
+    }
+
+    public void useSkills() {
+
+    }
+
+    public void sendApplicationCommunication(String JSON) {
+
+    }
+
+    public void performAction(int userID, int actionNumber, int betChip) {
+
+    }
+
+    public void sendCardInformation() {
+
+    }
+
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void dealInitialCard(int cardPerPlayer) {
+
+    }
+
+    public void showAllHands() {
+
+    }
+
+    /*
+    public void checkLostUser(ArrayList<Player> players, ArrayList<Player> rankList) {
+
+    }
+
+     */
+    public void addDisableHands(String hand) {
+
+    }
+
+    public void addUsedSkill(int skill) {
+
+    }
+
+    /*
+    public void checkPlayersList(ArrayList<Player> players) {
+
+    }
+
+     */
+
+
+}
