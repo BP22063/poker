daa707314423eecf6a9f7e9a3860dd1b5e9fbc62
bp22029 <bp22029@shibaura-it.ac.jp>
2024-12-24 16:46:49 +0900

    Actionクラスを追加

---
+++ src/main/java/org/example/Action.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Action {
+
+    public void executeBet(Player players, int actionNumber, int betChip) {
+
+    }
+
+    public void executePass(Player players, int actionNumber, int betChip) {
+
+    }
+
+    public void executeRaise(Player players, int actionNumber, int betChip) {
+
+    }
+
+    public void executeCall(Player players, int actionNumber, int betChip) {
+
+    }
+
+    public void executeDrop(Player players, int actionNumber, int betChip) {
+
+    }
+}
