4194d77a2dcb16573aed807fcc950c514edc4d7e
bp22069 <bp22069@shibaura-it.ac.jp>
2024-12-27 16:14:02 +0900

    Gameクラスの編集

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 
 public class Game {
 
-<<<<<<< HEAD
-    private static final int MAX_ROUND = 10;
-
-=======
->>>>>>> e7044ff (Gameクラスの追加、コンストラクタとgetterの設定)
     private int roundNum;
     private int gameID;
 
