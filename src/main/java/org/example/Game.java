fe372b9b6165dbbc867eb1298a90fc1f91da4c4e
bp22069 <bp22069@shibaura-it.ac.jp>
2024-12-27 15:34:45 +0900

    Gameクラスの追加、コンストラクタとgetterの設定

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Game {
+
+    private int roundNum;
+    private int gameID;
+
+    public Game(){
+        this.roundNum = 1;
+    }
+
+    public int getRoundNum(){
+        return this.roundNum;
+    }
+
+    public int getGameID(){
+        return this.gameID;
+    }
+
+    public void checkRound(int round){
+
+    }
+
+    public void progressRound(int round){
+
+    }
+
+    public void decideRankCutPlayer(){
+
+    }
+
+    public void renewRound(ArrayList<Player> rankList){
+
+    }
+
+    public void receiveNotification(){
+
+    }
+
+
+}
