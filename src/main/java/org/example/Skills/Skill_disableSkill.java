7e7e677aa1321f846ce983c4b3d700ef3ac21bc3
bp22069 <bp22069@shibaura-it.ac.jp>
2024-12-30 18:30:33 +0900

    Skillクラスの作成、コンストラクタとgetterの設定

---
+++ src/main/java/org/example/Skills/Skill_disableSkill.java
@@ -0,0 +1,15 @@
+package org.example.Skills;
+
+public class Skill_disableSkill {
+
+    private static final int skillID = 0;
+
+    public int getSkillID(){
+        return skillID;
+    }
+
+    public void useSkill_disableSkill(){
+
+    }
+
+}
---
+++ src/main/java/org/example/Skills/Skill_handSwap.java
@@ -0,0 +1,28 @@
+package org.example.Skills;
+
+import org.example.Player;
+
+public class Skill_handSwap {
+
+    private static final int skillID = 3;
+    Player player1;
+    Player player2;
+
+    // コンストラクタ
+    public Skill_handSwap( Player player1, Player player2 ){
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public int getSkillID(){
+        return skillID;
+    }
+
+    public void useSkill_handSwap(){
+
+    }
+
+    private void swapHand(){
+
+    }
+}
