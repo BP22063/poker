bf802132ef19723c760876e9389b6fe9b4a79d99
bp22069 <bp22069@shibaura-it.ac.jp>
2025-01-15 13:47:36 +0900

    adaptSkill(役無効関連)の変更

--- src/main/java/org/example/Dealer.java
+++ src/main/java/org/example/Dealer.java
@@ -154,7 +154,9 @@ public class Dealer {
             adaptSkill(player);
 
         } else if (args.length == 1) {
-            if (args[0] instanceof String) {
+            if(args[0] instanceof Double){
+                adaptSkill(player,(Double) args[0]);
+            }else if (args[0] instanceof String) {
                 adaptSkill(player, (String) args[0]);
 
             } else if (args[0] instanceof Integer) {
@@ -171,7 +173,7 @@ public class Dealer {
 
 
     // スキルの情報に応じてインスタンスを生成
-    private void adaptSkill(Player player){
+    private void adaptSkill(Player player,Double tmp){
         player.removeSkill(0);
         this.skills_disableSkill.add(new Skill_disableSkill());
     }
