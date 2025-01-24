8c31b91dfbf394f35acbd1fa9281167ad436100b
shunu <bp22030@shibaura-it.ac.jp>
2025-01-25 00:42:52 +0900

    Test

--- src/test/java/Skills/Skill_disableSkillTest.java
+++ src/test/java/Skills/Skill_disableSkillTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class Skill_disableSkillTest {
+public class Skill_disableSkillTest {
 
     @Test
     void testGetSkillID() {
